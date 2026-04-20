ea4808d42796f06fe73637c0ba9c0cb8ec479e06
Kroniak <me@kroniak.net>
2026-04-20 13:48:27 +0300

    feat(skills): update skills

--- .ai/skills-update.sh
+++ .ai/skills-update.sh
@@ -9,6 +9,5 @@ command -v npx >/dev/null 2>&1 || {
 SCRIPT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_ROOT"/..
 
-npx skills add obra/superpowers -a cursor -a codex --skill '*' -p -y &
 npx skills add https://github.com/vercel-labs/agent-skills -a cursor -a codex --skill vercel-react-best-practices -p -y &
 npx skills add https://github.com/anthropics/skills -a cursor -a codex --skill frontend-design -p -y
